1d28ed93795ee0b82878245a2f4258b5c9120151
Dominik Heinecke <dominikheinecke+github@icloud.com>
2024-07-15 22:02:43 +0200

    Homepage Angepasst Suchfeld hinzugefügt (non-funktional) Liste an Jobs wurde hinzugefügt

--- ASPNetCourse/Controllers/HomeController.cs
+++ ASPNetCourse/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using ASPNetCourse.Data;
 using ASPNetCourse.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,18 @@ namespace ASPNetCourse.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var allJobPostings = _context.JobPostings.ToList();
+            return View(allJobPostings);
         }
 
         public IActionResult Privacy()
